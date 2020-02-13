222c6f621a9ec00beda8b5bc64e94377a9513d96
Sergej Charskij <sergej.charskij@gmail.com>
2020-02-13 22:00:14 +0000

    Add an async file handler

--- src/service/cli/generate/generateOffers.js
+++ src/service/cli/generate/generateOffers.js
@@ -1,45 +1,9 @@
 'use strict';
 
-const {getRandomIntInclusive} = require(`../../../utils`);
+const {getRandomIntInclusive, readFileByName} = require(`../../../utils`);
 
 const Types = [`offer`, `sale`];
-const Titles = [
-  `Продам книги Стивена Кинга.`,
-  `Продам новую приставку Sony Playstation 5.`,
-  `Продам отличную подборку фильмов на VHS.`,
-  `Куплю антиквариат.`,
-  `Куплю породистого кота.`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`,
-];
-const Descriptions = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.,`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `Две страницы заляпаны свежим кофе.`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`,
-];
 const MAX_DESC_NUM = 5;
-const Categories = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 const Price = {min: 1000, max: 100000};
 
 
@@ -48,22 +12,26 @@ function getType(maxNum, type) {
   const descObj = new Set();
   Array(descNum).fill().forEach(() => {
     const key = getRandomIntInclusive(0, type.length - 1);
-    descObj.add(key);
+    descObj.add(type[key]);
   });
 
   return Array.from(descObj);
 }
 
-function generateOffers(count) {
+async function generateOffers(count) {
+  const titles = await readFileByName(`titles.txt`);
+  const descriptions = await readFileByName(`sentences.txt`);
+  const categories = await readFileByName(`categories.txt`);
+
   return Array(count).fill().map(() => {
     const picIndex = getRandomIntInclusive(1, 16);
     return ({
       "type": Types[getRandomIntInclusive(0, Types.length - 1)],
-      "title": Titles[getRandomIntInclusive(0, Titles.length - 1)],
-      "description": getType(MAX_DESC_NUM, Descriptions),
+      "title": titles[getRandomIntInclusive(0, titles.length - 1)],
+      "description": getType(MAX_DESC_NUM, descriptions),
       "sum": getRandomIntInclusive(Price.min, Price.max),
       "picture": `item${String(0 + picIndex).slice(-2)}`,
-      "category": getType(Categories.length - 1, Categories),
+      "category": getType(categories.length - 1, categories),
     });
   });
 }
