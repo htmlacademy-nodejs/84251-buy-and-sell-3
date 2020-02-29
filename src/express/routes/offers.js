129281584ca31b7f4384f9ad6a958a30d3454a0a
Sergej Charskij <sergej.charskij@gmail.com>
2020-02-29 19:13:14 +0000

    Add page templates

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const myOffers = new Router();
 
-myOffers.get(`/category/:id`, (req, res) => res.send(`/offers/category/${req.params.id}`));
-myOffers.get(`/add`, (req, res) => res.send(`/offers/add`));
-myOffers.get(`/edit/:id`, (req, res) => res.send(`offers/edit/${req.params.id}`));
-myOffers.get(`/:id`, (req, res) => res.send(`offers/${req.params.id}`));
+myOffers.get(`/category/:id`, (req, res) => res.render(`pages/category`));
+myOffers.get(`/add`, (req, res) => res.render(`pages/new-ticket`));
+myOffers.get(`/edit/:id`, (req, res) => res.render(`pages/ticket-edit`));
+myOffers.get(`/:id`, (req, res) => res.render(`pages/ticket`));
 
 module.exports = myOffers;
